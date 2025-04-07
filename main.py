db7625b16e2f48ba7d12691eb98d42dbc337fe51
NorzOman2 <arshaddj257@gmail.com>
2025-04-07 21:35:56 +0530

    Update main.py via AI assist

--- main.py
+++ main.py
@@ -6,6 +6,14 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')  # You'll need an about.html template
+
+@app.route('/contact')
+def contact():
+    return "Contact Us page goes here!" # Or render_template('contact.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
-hello
